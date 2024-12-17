f97fd27bb3dbbd60011f2e5471a237c4131557c8
Sunny Kumar <sk202542@gmail.com>
2024-12-17 14:41:13 +0530

    make react project]

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Footer from "./components/Footer/footer"
+import Header from "./components/Header/header"
+import Hero from "./components/heroSection/hero"
+import Main from "./components/main/main"
+import Main2 from "./components/main/main2"
+function App() {
+return (<div>
+<Header/>
+<Hero/>
+<Main/>
+<Main2/>
+<Footer/>
+</div>
+)}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+
+import { createRoot } from 'react-dom/client'
+
+import App from './App.jsx'
+import Style from "./main.module.css"
+
+createRoot(document.getElementById('root')).render(
+    <App />
+)
